dfdead7023ef718b70c5f5e5a7dc31afc995abbd
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2023-08-29 17:07:55 -0700

    Add files via upload

--- ejercicios/Ejercicios_2/ejercicio_2.1.py
+++ ejercicios/Ejercicios_2/ejercicio_2.1.py
@@ -1,18 +1,20 @@
 #pedir nombre y edad de los compañeros y ordenar los datos de mayor a menor
-
 #el mayor de la clase es el profesor y el menor el asistente
-
 profesor = ""
 Alumno_asistente = ""
-
-my_dict = {}
-
-
-for i in range(10):
+alumnos = {}
+edadadesyalumnos = []
+cantidad_alumnos = 0
+for i in range(3):
+    edad = int(input("Dame tu edad "))
     nombre = input("Dime tu nombre ")
-    edad = int(input("dime tu edad "))
-    i = nombre 
-    my_dict[i] = edad
-
-
-print(my_dict)
+    i = edad
+    alumnos[i] = nombre
+dic_it =alumnos.items()
+for o in dic_it:
+    edadadesyalumnos.append(o)
+edadadesyalumnos.sort(reverse=True)
+print(edadadesyalumnos)
+profesor = edadadesyalumnos[0]
+Alumno_asistente = edadadesyalumnos[-1]
+print(f"El profesor es {profesor} el asistente es {Alumno_asistente}")
