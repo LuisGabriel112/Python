6c99c949a3eb35b88b8e723dbc2f9cb12d5d7612
Luis Venegas <venegassaucedoluis@gmail.com>
2023-09-18 20:05:31 -0600

    empezando en backend

---
+++ Backend/typehints.py
@@ -0,0 +1,16 @@
+#Tipe hints
+
+my_string_variable = "String"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+My_Typed_Variable: str = "My Tiped variable"
+print(My_Typed_Variable)
+
+My_Typed_Variable = 5
+print(My_Typed_Variable)
+print(type(My_Typed_Variable))
